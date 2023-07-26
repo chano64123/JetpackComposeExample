2cf61b950d69ca319b4d3156d58ea7691e6bdd5a
chano64123 <promo201674@gmail.com>
2023-07-26 17:22:36 -0500

    Initial commit

---
+++ app/src/main/java/com/chano/jetpackcomposeexample/Columns.kt
@@ -0,0 +1,152 @@
+package com.chano.jetpackcomposeexample
+
+import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.node.modifierElementOf
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class Columns : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ViewContainer()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ViewContainer() {
+    Scaffold(
+        topBar = { ToolBar() },
+        floatingActionButton = { FAB() },
+        floatingActionButtonPosition = FabPosition.End
+    ){x ->
+        Content()
+    }
+}
+
+@Preview
+@Composable
+fun FAB() {
+    val context = LocalContext.current
+    FloatingActionButton(onClick = {
+        Toast.makeText(context, "Que fue mano???", Toast.LENGTH_SHORT).show()
+    }) {
+        Text(text = "X")
+    }
+}
+
+@Preview
+@Composable
+fun ToolBar() {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Chano Profile",
+                color = Color.White
+            )
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = colorResource(id = R.color.background))
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Content() {
+    var counter by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray)
+            .padding(16.dp)
+    ) {
+        item {
+            Image(
+                painter = painterResource(id = R.drawable.hotel),
+                contentDescription = "Profile photo",
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            Row(modifier = Modifier.padding(top = 8.dp)) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_favorite),
+                    contentDescription = "like",
+                    modifier = Modifier.clickable {
+                        counter++
+                    }
+                )
+                Text(
+                    text = counter.toString(),
+                    color = Color.White,
+                    modifier = Modifier.padding(start = 4.dp)
+                )
+            }
+            Text(
+                text = "Chano",
+                fontSize = 32.sp,
+                color = Color.White,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = "Hola",
+                color = Color.White,
+                modifier = Modifier.padding(top = 2.dp)
+            )
+            Text(
+                text = "Como estas????",
+                color = Color.White,
+                modifier = Modifier.padding(top = 2.dp)
+            )
+            LazyRow(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 2.dp)
+            ) {
+                item {
+                    Text(text = "Stack:", color = Color.White)
+                    Text(text = "C#", color = Color.White)
+                    Text(text = "Type Script", color = Color.White)
+                    Text(text = "Kotlin", color = Color.White)
+                }
+            }
+        }
+    }
+}
